2b148ecdc73edaca64f1c462f8e1b6ca45bcf5c3
leggetter <phil@leggetter.co.uk>
2015-06-10 12:09:26 +0100

    Adding NoSleep.js

--- js/app.js
+++ js/app.js
@@ -419,4 +419,15 @@
     storage.clear();
   };
   
+  var noSleep = new NoSleep();
+
+  function enableNoSleep() {
+    noSleep.enable();
+    document.removeEventListener('touchstart', enableNoSleep, false);
+  }
+
+  // Enable wake lock.
+  // (must be wrapped in a user input event handler e.g. a mouse or touch handler)
+  document.addEventListener('touchstart', enableNoSleep, false);
+  
 })();
